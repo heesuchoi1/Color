88401e09a0cbf87575fd82d503bf764cf9c944de
godaeun <godaeun@naver.com>
2014-09-18 15:37:06 +0900

    32

--- color.js
+++ color.js
@@ -2,10 +2,8 @@ function setup(){
 	createCanvas(640,480);
 	backgroundColor = color(0,0,0);
 	fillColor = color(0,0,255);
-}
 
-function draw() {
-	background(backgroundColor);
+		background(backgroundColor);
 	fillColor = color(Math.random() * 255,
 		Math.random() * 255,
 		Math.random() * 255,
@@ -20,5 +18,9 @@ mouseColor = color(mouseX / 640 * 255,
 	//rect(100,100,100,100);
 	fill(fillColor);
 	ellipse(300,300,100,100);
+}
+
+function draw() {
+
 
 }
